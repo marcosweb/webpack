e6d52eb3467a0dcba18c1c2206e5d6b40d8c7216
= <=>
2022-09-02 08:05:25 -0300

    -m

---
+++ src/formulario.js
@@ -0,0 +1,17 @@
+import { Button } from "bootstrap";
+import React from "react";
+
+class Formulario extends React.Component {
+
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <Button>Bom dia</Button>
+        )
+    }
+}
+
+export default Formulario
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom'
 import 'bootstrap'
 import './estilo.scss'
 import { bomDia } from './nayra.js'
+import Formulario from './formulario'
 
 const mensagem = bomDia('Bolsonaro')
 
@@ -11,6 +12,7 @@ ReactDOM.render(
     <div className="react">
         <h2>Isto aqui é REACT</h2>
         <p>{mensagem}</p>
+        <Formulario />
     </div>,
     document.querySelector('#app')
 )
